08ebfa31784c3d6b97d5418e721014659dc03c3c
Duma Vlad <vlad.duma@yahoo.co.uk>
2022-12-11 00:13:19 +0200

    Added metamask, home page and add page

---
+++ normal_dev/mern/mop/src/components/Home.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Button, Card } from "react-bootstrap";
+import Image from "react-bootstrap/Image";
+import "../css/home.css"
+import CryptoImg from '../assets/images/crypto.png'
+import { Link } from 'react-router-dom'
+
+
+
+
+const Home = () => {
+
+    return (
+        <div className='home'>
+            <Image className="img-fluid" src={CryptoImg} rounded />
+            <Card className="my-auto text-center" >
+                <Card.Body className="card-body">
+                    {/* <Card.Title>Special title treatment</Card.Title> */}
+                    <Card.Text>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda fuga itaque minus rerum error voluptatum recusandae necessitatibus sequi consectetur in, aut dicta dolor unde soluta maxime dolores libero impedit placeat porro labore, odio explicabo, dolore vero culpa. Veritatis possimus explicabo recusandae ex eaque fugit iste quo libero! Sapiente, eius consequuntur.
+                    </Card.Text>
+                    <Link to='/create'>
+                        <Button variant="primary">
+                            Get Started
+                        </Button>
+                    </Link>
+                </Card.Body>
+            </Card>
+        </div>
+    );
+
+
+}
+
+export default Home;

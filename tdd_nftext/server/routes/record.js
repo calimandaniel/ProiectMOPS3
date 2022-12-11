845d8b659ebc9908d6f62ba54ce470f293b851f3
Botezatu Daniela <danielabotezatu00@gmail.com>
2022-12-11 22:31:59 +0200

    Add documents page and tests

--- tdd_nftext/server/routes/record.js
+++ tdd_nftext/server/routes/record.js
@@ -11,7 +11,6 @@ const dbo = require("../db/conn");
 // This help convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
 
-
 // This section will help you get a list of all the records.
 recordRoutes.route("/record").get(function (req, res) {
   let db_connect = dbo.getDb("employees");
@@ -28,12 +27,18 @@ recordRoutes.route("/record").get(function (req, res) {
 recordRoutes.route("/record/:id").get(function (req, res) {
   let db_connect = dbo.getDb();
   let myquery = { _id: ObjectId(req.params.id) };
-  db_connect
-    .collection("records")
-    .findOne(myquery, function (err, result) {
-      if (err) throw err;
-      res.json(result);
-    });
+  db_connect.collection("records").findOne(myquery, function (err, result) {
+    if (err) throw err;
+    res.json(result);
+  });
+});
+
+// This section will help you get a single record by name
+recordRoutes.route("/record/filter/name/:name").get(async function (req, res) {
+  let db_connect = dbo.getDb();
+  let myquery = { name: req.params.name };
+  const filtered = await db_connect.collection("records").find(myquery).toArray();
+  res.json(filtered);
 });
 
 // This section will help you create a new record.
@@ -43,7 +48,7 @@ recordRoutes.route("/record/add").post(function (req, response) {
     name: req.body.name,
     title: req.body.title,
     description: req.body.description,
-    text: req.body.text
+    text: req.body.text,
   };
   db_connect.collection("records").insertOne(myobj, function (err, res) {
     if (err) throw err;
@@ -60,16 +65,14 @@ recordRoutes.route("/update/:id").post(function (req, response) {
       name: req.body.name,
       title: req.body.title,
       description: req.body.description,
-      text: req.body.text
+      text: req.body.text,
     },
   };
-  db_connect
-    .collection("records")
-    .updateOne(myquery, newvalues, function (err, res) {
-      if (err) throw err;
-      console.log("1 document updated");
-      response.json(res);
-    });
+  db_connect.collection("records").updateOne(myquery, newvalues, function (err, res) {
+    if (err) throw err;
+    console.log("1 document updated");
+    response.json(res);
+  });
 });
 
 // This section will help you delete a record

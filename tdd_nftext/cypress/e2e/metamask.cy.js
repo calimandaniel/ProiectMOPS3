db9eef232cdc7ae4ca52cc37cf09165267999eca
Botezatu Daniela <danielabotezatu00@gmail.com>
2022-12-11 23:03:32 +0200

    Setup cypress metamask testing

---
+++ tdd_nftext/cypress/e2e/metamask.cy.js
@@ -0,0 +1,19 @@
+describe("User can load page", () => {
+  // before(() => {
+  //   cy.setupMetamask();
+  //   cy.changeMetamaskNetwork("localhost");
+  //   cy.visit("/add");
+  // });
+
+  it("is expected to display the local wallet address", () => {
+    cy.visit("http://localhost:3000/add");
+    cy.contains("button", "Connect to MetaMask");
+  });
+
+  it("show form after connect", () => {
+    cy.visit("http://localhost:3000/add");
+    cy.get("button").click();
+    cy.wait(5000);
+    cy.contains("p","Form");
+  });
+});
